d152ebb0e669c35887f47c9c0b063d46c8e78254
fatma sayan <sayanfatma.001@gmail.com>
2024-06-10 00:28:04 +0300

    wrepo

--- Core/Api.Application/Interface/Repositories/IReadRepository.cs
+++ Core/Api.Application/Interface/Repositories/IReadRepository.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Query;
 using System.Linq.Expressions;
 
-namespace Api.Application.Interface.Repositories
+namespace Api.Application.Interface.Repositories;
 {
     public interface IReadRepository<T> where T : class, IEntityBase, new()
     {
--- Core/Api.Application/Interface/Repositories/IWriteRepository.cs
+++ Core/Api.Application/Interface/Repositories/IWriteRepository.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Api.Domain.Comman;
 
-namespace Api.Application.Interface.Repositories
+namespace Api.Application.Interface.Repositories;
 {
-    public interface IWriteRepository
+    public interface IWriteRepository<T> where T : class, IEntityBase, new()
     {
+        // Save işlemi yaptıktan sonra save işlemi 1 den küçük olma durumuna göre başarılı oluşunu anlayabililirim.
+        Task<int> AddAsync(T entity);
+        Task<IList<int>> AdRangeAsync(IList<T> entity);
     }
 }
